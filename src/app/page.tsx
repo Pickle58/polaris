4fed4fb354ab9b93a0094c07dac6c82d6ea21ec8
James Pilkington <jpilkington332@gmail.com>
2026-03-08 06:34:36 -0700

    03: database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
-import Image from "next/image";
+"use client";
+
+import { useMutation, useQuery } from "convex/react";
+import { api } from "../../convex/_generated/api";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
+  const projects = useQuery(api.projects.get);
+  const createProject = useMutation(api.projects.create);
   return (
-    <div>
-      Hello Pickle
+    <div className="flex flex-col gap-2">
+      <Button onClick={() => createProject({ name: "New Project" })}>
+        Add new
+      </Button>
+      {projects?.map((project) =>
+        <div 
+        className="border rounded p-2 flex flex-col"
+        key={project._id}>
+          <p>{project.name}</p>
+          <p>Owner ID: {project.ownerId}</p>
+        </div>
+      )}
     </div>
   );
 }
